f361c2730d5a7f253f0fb33257f22460e7464533
Ibrahim Gokalp <gokalpi@gmail.com>
2019-12-26 10:31:04 +0300

    Enhanced documentation of student controller

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentApp.Data;
 using StudentApp.Models;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace StudentApp.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class StudentsController : ControllerBase
@@ -19,15 +21,26 @@ namespace StudentApp.Controllers
             _context = context;
         }
 
-        // GET: api/Students
+        /// <summary>
+        /// Gets a list of all students
+        /// </summary>
+        /// <returns>List of students</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Student>>> GetStudent()
         {
             return await _context.Student.ToListAsync();
         }
 
-        // GET: api/Students/5
+        /// <summary>
+        /// Gets a specific student's details
+        /// </summary>
+        /// <param name="id">Student id</param>
+        /// <returns>Student details</returns>
+        /// <response code="400">If the student id is not a valid number</response>
+        /// <response code="404">If the student is not found</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Student>> GetStudent(int id)
         {
             var student = await _context.Student.FindAsync(id);
@@ -40,11 +53,40 @@ namespace StudentApp.Controllers
             return student;
         }
 
-        // PUT: api/Students/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
+        /// <summary>
+        /// Updates a specific student
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /api/Students
+        ///     {
+        ///       "id": 1,
+        ///       "name": "Ibrahim Gokalp",
+        ///       "email": "gokalpi@gmail.com",
+        ///       "phone": "+90-xxx-xxxxxxx",
+        ///       "gender": "Male",
+        ///       "bloodGroup": "XX",
+        ///       "address": {
+        ///         "street": "XXXX",
+        ///         "city": "Istanbul",
+        ///         "state": "TR",
+        ///         "country": "Turkey"
+        ///       }
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="id">Student id</param>
+        /// <param name="student">Updated student details</param>
+        /// <returns></returns>
+        /// <response code="204">Returns no content if successful</response>
+        /// <response code="400">If the student id is different than id in content</response>
+        /// <response code="404">If the student is not found</response>
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutStudent(int id, Student student)
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateStudent(int id, Student student)
         {
             if (id != student.Id)
             {
@@ -72,11 +114,37 @@ namespace StudentApp.Controllers
             return NoContent();
         }
 
-        // POST: api/Students
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
+        /// <summary>
+        /// Adds a new student
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /api/Students
+        ///     {
+        ///       "id": 0,
+        ///       "name": "Ibrahim Gokalp",
+        ///       "email": "gokalpi@gmail.com",
+        ///       "phone": "+90-xxx-xxxxxxx",
+        ///       "gender": "Male",
+        ///       "bloodGroup": "XX",
+        ///       "address": {
+        ///         "street": "XXXX",
+        ///         "city": "Istanbul",
+        ///         "state": "TR",
+        ///         "country": "Turkey"
+        ///       }
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="student">Student details</param>
+        /// <returns>Created student</returns>
+        /// <response code="201">Returns the created student</response>
+        /// <response code="400">If the student is null</response>
         [HttpPost]
-        public async Task<ActionResult<Student>> PostStudent(Student student)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Student>> AddStudent(Student student)
         {
             _context.Student.Add(student);
             await _context.SaveChangesAsync();
@@ -84,8 +152,18 @@ namespace StudentApp.Controllers
             return CreatedAtAction("GetStudent", new { id = student.Id }, student);
         }
 
-        // DELETE: api/Students/5
+        /// <summary>
+        /// Deletes a specific student
+        /// </summary>
+        /// <param name="id">Student id</param>
+        /// <returns>Deleted student</returns>
+        /// <response code="200">Returns the deleted student</response>
+        /// <response code="400">If the id is null</response>
+        /// <response code="404">If the student is not found</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Student>> DeleteStudent(int id)
         {
             var student = await _context.Student.FindAsync(id);
